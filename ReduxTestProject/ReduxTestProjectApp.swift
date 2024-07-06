66c852948f2a8a8309d99ff4d5537c98295b9d7b
Nebo <antrabot@gmail.com>
2024-07-06 20:34:39 +0300

    Initial Commit

---
+++ ReduxTestProject/ReduxTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReduxTestProjectApp.swift
+//  ReduxTestProject
+//
+//  Created by Nebo on 06.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ReduxTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
